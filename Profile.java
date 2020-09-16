c7c778d5b39e6c4b4bcd5811477dfafbdd630dd2
johannesalmendo <johannesalmendo@student.telkomuniversity.ac.id>
2020-09-16 18:10:27 +0700

    Profile.java

---
+++ Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+	    
+	public static void main(String[] args){
+		System.out.println("Nama  : Johannes");
+		System.out.println("NIM   : 6706204079");
+		System.out.println("Kelas : D3IF44-04");
+	}
+}
